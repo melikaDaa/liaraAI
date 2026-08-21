b399ff9512681dea25349dff0a37a1c59240fb39
melikaDaa <129381008+melikaDaa@users.noreply.github.com>
2026-08-21 18:43:56 +0330

    add liara config for depoly

--- src/LiaraAI.Api/obj/Debug/net9.0/LiaraAI.Api.AssemblyInfo.cs
+++ src/LiaraAI.Api/obj/Debug/net9.0/LiaraAI.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LiaraAI.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6f457c329609c83b66e9babf37db9e6b259102b9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ce8bc43a3308dd59ea5b1ef0aa83a2d17df112b1")]
 [assembly: System.Reflection.AssemblyProductAttribute("LiaraAI.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LiaraAI.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
